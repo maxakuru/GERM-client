d9b6e41cb73105b7b788982b262e3444eab2e463
Max Edell <mgedell@gmail.com>
2018-03-18 15:33:42 -0400

    push content

---
+++ src/App.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createHttpLink } from 'apollo-link-http';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { ReduxCache, apolloReducer } from 'apollo-cache-redux';
+import ReduxLink from 'apollo-link-redux';
+const URL = 'localhost:8080'; // set your comp's url here
+const store = createStore(
+  combineReducers({
+    apollo: apolloReducer,
+  }),
+  {}, // initial state
+  composeWithDevTools(),
+);
+const cache = new ReduxCache({ store });
+const reduxLink = new ReduxLink(store);
+const httpLink = createHttpLink({ uri: `http://${URL}/graphql` });
+const link = ApolloLink.from([
+  reduxLink,
+  httpLink,
+]);
+export const client = new ApolloClient({
+  link,
+  cache,
+});
+
+const instructions = Platform.select({
+  ios: 'Press Cmdd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <Provider store={store}>
+          <View style={styles.container}>
+            <Text style={styles.welcome}>
+              Welcome to React Native!
+            </Text>
+            <Text style={styles.instructions}>
+              To get started, edit App.js
+            </Text>
+            <Text style={styles.instructions}>
+              {instructions}
+            </Text>
+          </View>
+        </Provider>
+      </ApolloProvider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
